526b7620391832e1774a8d9ba999aa0774006328
siriusSupreme <guoplc@163.com>
2017-10-14 15:52:58 +0800

    ''

---
+++ src/Exceptions/BindingResolutionException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Sirius\Container\Exceptions;
+
+use Exception;
+use Psr\Container\ContainerExceptionInterface;
+
+class BindingResolutionException extends Exception implements ContainerExceptionInterface
+{
+    //
+}
---
+++ src/Exceptions/EntryNotFoundException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Sirius\Container\Exceptions;
+
+use Exception;
+use Psr\Container\NotFoundExceptionInterface;
+
+class EntryNotFoundException extends Exception implements NotFoundExceptionInterface
+{
+    //
+}
